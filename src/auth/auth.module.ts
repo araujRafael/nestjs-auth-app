094305cbdf974228eff87291de2c445b03685796
Rafael Diniz <araujodiniz1994@gmail.com>
2024-02-14 10:09:01 -0300

    feat:: localStrategy validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   exports: [AuthService]
 })
 export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt'
 @Injectable()
 export class AuthService {
-    login() {
-        throw new Error('Method not implemented.');
-    }
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
-    }
+	constructor(private readonly userService: UserService) { }
+	async login() {
+		return { token: '' }
+	}
+	async validateUser(email: string, password: string) {
+		const user = await this.userService.findByEmail(email)
+		if (user) {
+			const isValidPass = await bcrypt.compare(password, user.password)
+			if (isValidPass) {
+				return {
+					...user,
+					password: undefined
+				}
+			}
+		}
+		throw new Error('Email address or password provided is invalid.')
+	}
 }
