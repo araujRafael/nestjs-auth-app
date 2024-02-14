23aaf86f7f7473895a3287ab5058f362cda00a9e
Rafael Diniz <araujodiniz1994@gmail.com>
2024-02-14 09:06:49 -0300

    add create user and add auth modules, controller, service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks()
+  // Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
+
 bootstrap();
---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get('/:email')
+  findByEmail(@Param() { email }: { email: string }) {
+    return this.userService.findByEmail(email)
+  }
+}
